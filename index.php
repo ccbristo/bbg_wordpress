c7dc2d1153208eba5a81f60460632f4923c54c74
Chris Bristol <Chris Bristol>
2017-03-05 10:25:29 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-us" lang="en-us" dir="ltr" >
+<head>
+	<link rel="stylesheet" href="css/site.css" type="text/css" />
+</head>
+<body>
+	<div id="fb-root"></div>
+	<script type="text/javascript">(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	</script>
+	<div class="wrapper">
+		<table cellspacing="0" cellpadding="0" border="0" style="width: 100%">
+			<tr>
+				<td style="background-color: #006633">
+					<a id="rootLink" href="www.battlegroundbrewers.com">
+						<table cellspacing="0" cellpadding="0" border="0" style="width: 100%">
+							<tr>
+								<td>
+									<img src="images/bbg_logo.gif"  alt="Battleground Brewers Guild Logo" />
+								</td>
+								<td>
+									<span class="SiteTitle">
+										Battleground Brewers Guild
+									</span>
+									<br />
+									<div class="SubHeader">
+										Homebrewing Greensboro&apos;s Best Beer in the 21st Century
+									</div>
+								</td>
+							</tr>
+						</table>
+					</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div id="ContentWrapper">
+						<table style="width: 100%">
+							<tr>
+								<td colspan="3" class="MenuContainer">
+									&nbsp;
+								</td>
+							</tr>
+							<tr>
+									<td class="Sidebar SidebarLeft">
+										Coming Soon!
+									</td>
+								<td class="MainContent">
+									<!--<jdoc:include type="message" />
+									<jdoc:include type="component" />-->
+									Please excuse the mess as we bring our site back up!
+
+									In the mean time, please check out our <a href="https://www.facebook.com/BattlegroundBrewersGuild">official Facebook page</a> or start a discussion in our <a href="https://www.facebook.com/groups/984018835050442/">Facebook Group<a>!
+								</td>
+									<td class="Sidebar SidebarRight">
+										<!--<jdoc:include type="modules" name="right" />-->
+										<div style="margin: 12px 2px 6px 2px;">
+											<strong>Calendar of Events<strong>
+											<iframe src="https://calendar.google.com/calendar/embed?title=BBG%20Calendar&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=250&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=battlegroundbrewersguild%40gmail.com&amp;color=%23AB8B00&amp;ctz=America%2FNew_York"
+											style="border:solid 1px #777" width="250" height="400" frameborder="0" scrolling="no"></iframe>
+										
+											<div class="fb-like" data-href="http://www.facebook.com/BattlegroundBrewersGuild" data-send="false" data-width="100%" data-show-faces="true"></div>
+										</div>
+									</td>
+							</tr>
+						</table>
+					</div>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div class="footer">
+		Copyright &copy; 2010-present Battleground Brewers Guild All Rights Reserved
+	</div>
+</body>
+</html>
